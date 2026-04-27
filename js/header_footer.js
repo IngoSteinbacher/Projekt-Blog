8883cbc19447d9d57d7e0bb8e3a71c6236150792
Ingo S. <ingo35@gmx.net>
2026-04-27 17:16:35 +0200

    Index Slider dynamic

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Header laden
+    
     fetch("header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-placeholder").innerHTML = data;
         });
 
-    // Footer laden
+    
     fetch("footer.html")
         .then(response => response.text())
         .then(data => {
